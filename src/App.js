d15818e83eb3b5696f723f41ab116c85602fe0af
Anouar Dhahri <anouar.dhahri1@gmail.com>
2022-08-05 00:05:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import i18next from 'i18next'
+import Cookies from 'js-cookie'
+import reactLogo from './assets/react.png'
 
 function App() {
+
+  const [lang, setLang] = useState('');
+
+  const languages = [
+    {
+      code: 'en',
+      name: 'English',
+    },
+    {
+      code: 'fr',
+      name: 'Français',
+    },
+    {
+      code: 'ar',
+      name: 'العربية',
+      dir: 'rtl',
+    },
+  ]
+
+  const currentLanguageCode = Cookies.get('i18next') || lang || 'en'
+  const currentLanguage = languages.find((l) => l.code === currentLanguageCode)
+  const { t } = useTranslation()
+
+  useEffect(() => {
+    console.log('Setting page stuff')
+    document.body.dir = currentLanguage.dir || 'ltr'
+  }, [currentLanguage, t])
+
+  const handleLanguage = () => {
+    console.log(lang)
+    i18next.changeLanguage(lang)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='header'>
+        <h1>{t('title')}</h1>
+        <select onChange={(e) => setLang(e.target.value)} onClick={handleLanguage}>
+          {languages.map(({code, name }) => (
+            <option key={code} value={code}> {name}</option>
+          ))}
+          
+        </select>
+      </div>
+      <img src={reactLogo} alt='react' className='logo'/>
+      <div className='txt'>
+        <article>
+          {t('p1')}
+        </article>
+        <article>
+          {t('p2')}
+        </article>
+        <h2>{t('bnf')}</h2>
+        <article>
+        {t('bnft')}
+        </article>
+
+        <article>
+          <h4>{t('bn1h4')} </h4>
+          {t('bn1Exp')}
+        </article>
+
+        <article>
+          <h4>{t('bn2h4')} </h4>
+          {t('bn2Exp')}
+        </article>
+
+        <article>
+          <h4>{t('bn3h4')} </h4>
+          {t('bn3Exp')}
+        </article>
+
+        <article>
+          <h4>{t('bn4h4')} </h4>
+          {t('bn4Exp')}
+        </article>
+
+        <article>
+          <h4>{t('bn5h4')} </h4>
+          {t('bn5Exp')}
+        </article>
+
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import i18next from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import HttpApi from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+
+i18next
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs:['en', 'fr', 'ar'],
+    fallbackLng:'en',
+    debug: false,
+
+    detection: {
+      order: ['path', 'cookie', 'htmlTag'],
+      caches: ['cookies']
+    },
+    backend:{
+      loadPath:'/assets/locales/{{lng}}/translation.json'
+    }
+  })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
